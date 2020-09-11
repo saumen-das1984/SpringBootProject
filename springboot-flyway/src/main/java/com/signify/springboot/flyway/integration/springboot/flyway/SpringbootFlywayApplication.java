fd2b7f0755c26b791939eb859545ba326f44844c
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2020-09-11 12:12:24 +0530

    Spring Boot Flyway Integration
    Spring Boot Flyway Integration

---
+++ springboot-flyway/src/main/java/com/signify/springboot/flyway/integration/springboot/flyway/SpringbootFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.signify.springboot.flyway.integration.springboot.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFlywayApplication.class, args);
+	}
+
+}
